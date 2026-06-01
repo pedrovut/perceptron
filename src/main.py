6796c741f6143278cfbbf006cfc4daae9f549951
pedro <pedropereiradesouza.0411@gmail.com>
2026-06-01 15:34:56 -0400

    finalizando comentários, traduções e adicionando bias ao modelo

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from perceptron import Perceptron
 
-entradas = [ [0,0], [1,0], [0,1], [1,1] ]
-saidas = [[0], [0], [0], [1]]
+entradas = [ [0,0], [1,0], [0,1], [1,1] ] # Array de entradas
+saidas = [ [0], [0], [0], [1] ] # Array de possíveis saídas
 
-percp = Perceptron()
-pesos = percp.treinar(entradas, saidas, 0.01, 100)
+percep = Perceptron() # Cria nova instãncia (objeto) da classe Perceptron
+pesos = percep.treinar(entradas, saidas, 0.01, 100) # Usa a função treinar() e usa os arrays entradas, saídas (para entrada e saída)
+                                                    #  e os valores 0.01 e 100 (para LR e épocas) como parâmetros para a variável pesos receber
 
-print(percp.prever(pesos, 1, 1))
+print(percep.prever(pesos, 1, 1)) # Exibe no console o retorno da função prever(), com a variável pesos (para definir os pesos)
+                                  # e as entradas x,x (para definir entrada1 e entrada2) como parâmetros
--- src/perceptron.py
+++ src/perceptron.py
@@ -1,32 +1,33 @@
 import numpy as np
 
 class Perceptron:
-    def treinar(self, entradas, saidas, taxa_aprendizagem, epocas):
+    def treinar(self, entradas, saidas, taxa_aprendizagem, epocas): # Cria a função treinar com os parâmetros entradas, saídas, taxa de aprendizagem e a quantidade de épocas
         self.entradas = entradas
         self.saidas = saidas
         self.taxa_aprendizagem = taxa_aprendizagem
         self.epocas = epocas
 
-        peso1, peso2 = np.random.uniform(-1, 1),np.random.uniform(-1, 1) # Define peso1 e peso2 como um número aleatório entre -1 e 1 (uniformemente)
+        peso1, peso2, bias = np.random.uniform(-1, 1),np.random.uniform(-1, 1), np.random.uniform(-1, 1) # Define peso1, peso2 e bias como um número aleatório entre -1 e 1 (uniformemente)
 
         for _ in range(epocas): # Percorre cada época
             for j in range(len(entradas)): # Para cada coluna (j) em total(entrdas)
 
-                z = (entradas[j][0] * peso1) + (entradas[j][1] * peso2) # Soma das multiplicações
+                z = (entradas[j][0] * peso1) + (entradas[j][1] * peso2) + bias # Soma das multiplicações + bias
 
-                prev = 1 if z > 0.5 else 0 # Função de ativação (Step Function)
+                prev = 1 if z >= 0 else 0 # Função de ativação (Step Function)
 
                 erro = saidas[j][0] - prev # Cálculo do erro: erro = saída_esperada - previsão
 
                 peso1 = peso1 + (taxa_aprendizagem * erro * entradas[j][0]) # Novo peso 1
                 peso2 = peso2 + (taxa_aprendizagem * erro * entradas[j][1]) # Novo peso 2
+                bias = bias + (taxa_aprendizagem * erro) # Novo peso bias
 
                 print(z) # Exibe uma nova "soma das multiplicações" (z) no console
 
-        return peso1, peso2 # Função treinar() retorna novos pesos
+        return peso1, peso2, bias # Função treinar() retorna novos pesos
     
     def prever(self, pesos, entrada1, entrada2):
-        z =  (entrada1 * pesos[0]) + (entrada2 * pesos[1]) # 
-        prev = 1 if z > 0.5 else 0
+        z =  (entrada1 * pesos[0]) + (entrada2 * pesos[1]) + pesos[2] # Soma das multiplicações + bias (com os pesos definidos na função treinar)
+        prev = 1 if z >= 0 else 0 # Função de ativação definitiva
 
-        return prev
+        return prev # Função prever() retorna o resultado da Step Function
